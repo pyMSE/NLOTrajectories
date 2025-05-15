01a135fb7c41eb2ec35c7cef8eede2497934282c
Mariia Iavorskaia <all.mails.are.busy@gmail.com>
2025-05-15 12:05:33 +0200

    add unicycle 2nd order dynamic

--- src/nlotrajectories/core/dynamics.py
+++ src/nlotrajectories/core/dynamics.py
@@ -8,6 +8,7 @@ class Dynamics(str, Enum):
     POINT_1ST = "point_1st"
     POINT_2ND = "point_2nd"
     UNICYCLE = "unicycle"
+    UNICYCLE_2ND = "unicycle_2nd"
 
 
 class IRobotDynamics(ABC):
@@ -70,8 +71,32 @@ class Unicycle(IRobotDynamics):
         return ca.vertcat(dx, dy, dtheta)
 
 
+class Unicycle2ndOrder(IRobotDynamics):
+    def state_dim(self) -> int:
+        return 5  # x, y, theta, v, omega
+
+    def control_dim(self) -> int:
+        return 2  # a (linear acceleration), alpha (angular acceleration)
+
+    def dynamics(self, x: ca.MX, u: ca.MX) -> ca.MX:
+        theta = x[2]
+        v = x[3]
+        omega = x[4]
+        a = u[0]
+        alpha = u[1]
+        
+        dx = v * ca.cos(theta)
+        dy = v * ca.sin(theta)
+        dtheta = omega
+        dv = a
+        domega = alpha
+
+        return ca.vertcat(dx, dy, dtheta, dv, domega)
+
+
 DYNAMICS_CLASS_MAP = {
     Dynamics.POINT_1ST: PointMass1stOrder,
     Dynamics.POINT_2ND: PointMass2ndOrder,
     Dynamics.UNICYCLE: Unicycle,
+    Dynamics.UNICYCLE_2ND: Unicycle2ndOrder,
 }
